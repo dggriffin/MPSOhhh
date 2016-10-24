a6da9ed6bcf878a8d4a165b3def3b49576940e7a
unknown <ggriffin@DEVGGRIFFIN.vistaprint.net>
2016-10-24 17:18:48 -0400

    Add first mock example. Add next page and animations.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,17 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
+import testImg from '../images/Quantity_high_2.png';
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className="mpso">
+          <img src={testImg}/>
+          <button className="next hvr-buzz">Next</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
+import { Router, Route, browserHistory } from 'react-router'
+
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>, document.getElementById('app'));
